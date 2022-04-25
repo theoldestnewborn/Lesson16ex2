09f14d4f1ba51b4514fc9f04428cebdb465bcdd9
Aleksei <iamviga@yandex.ru>
2022-04-25 17:32:22 +0300

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -9,16 +9,22 @@ public class Main {
        intList = List.of(1,2,6,6,2,3,41,53,51,3,123);
        List<String> stringList = new ArrayList<>();
        stringList = List.of("string1","string2","3string", "4string", "strings", "s-string");
-
     }
 
     public List<String > sortList (ArrayList <String> tempStringList, Comparator <?> comparator,
-                                        int elementsAmmount, int page) {
+                                        int elementsAmount, int page) {
         List<String> newList = tempStringList.stream()
-                                .sorted(comparator)
-                                    .skip(elementsAmmount*page)
-                                        .limit(elementsAmmount)
+                                .sorted((Comparator<? super String>) comparator)
+                                    .skip(elementsAmount*page)
+                                        .limit(elementsAmount)
                                             .collect(Collectors.toList());
     return newList;
     }
 }
+//1. Создать в классе с методом main два неотсортированных списка со строками и числами.
+//        2. Написать обобщенный метод, который на вход принимает список, компаратор
+//        и два целых числа количество элементов на странице и номер странице.
+//        Метод должен с помощью стримов отсортировать список с помощью компаратора.
+//        И вернуть список из элементов количество которых задается первым числом.
+//        А начало брать с помощью отступа (номер страницы * количество элементов).
+//        3. В методе main с помощью цикла вывести постранично эти списки.
